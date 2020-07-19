34333244b3649ae35b9d63debfba1481d8b3c1e0
JAGJEEVAN GOPIREDDY <jgopireddy@JAGJEEVANs-MacBook-Pro.local>
2020-07-18 21:04:03 -0400

    updates

--- src/main/java/com/microservice/transactionservice/resource/TransactionServiceController.java
+++ src/main/java/com/microservice/transactionservice/resource/TransactionServiceController.java
@@ -1,5 +1,10 @@
 package com.microservice.transactionservice.resource;
 
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class TransactionServiceController {
 
 	@GetMapping("/message")
-	public String test() {
+	public String test(HttpServletRequest request, HttpServletResponse response) {
+		Enumeration<String> headers = request.getHeaderNames();
+		while(headers.hasMoreElements()) {
+			String headerKey = headers.nextElement();
+			String headerValue = request.getHeader(headerKey);
+			System.out.println(headerKey +" : "+headerValue);
+		}
 		return "Hello from Transaction-Service Service";
 	}
 }
